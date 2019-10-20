9fb41ef2fce15eedf72e4bdcc8eef3a73bc9da95
sbx_user1051 <sbx_user1051@169.254.68.53>
2019-10-20 22:18:05 +0000

    Sun Oct 20 22:18:05 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -2,9 +2,41 @@ package befaster.solutions.CHL;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckliteSolution {
+
+    private static Map<String, Integer> mapRegular = new HashMap<>();
+    private static Map<String, Integer> mapSpecial = new HashMap<>();
+
+    static {
+        mapRegular.put("A", 50);
+        mapRegular.put("B", 30);
+        mapRegular.put("C", 20);
+        mapRegular.put("D", 15);
+
+        mapSpecial.put("3A", 130);
+        mapSpecial.put("2B", 45);
+    }
+
     public Integer checklite(String skus) {
-        return 0;
+        if (validateInput(skus)) {
+            int result = 0;
+            for (Character c : skus.toCharArray()) {
+                result += mapRegular.get(c);
+            }
+        }
+        return -1;
+    }
+
+    private boolean validateInput(String skus) {
+        String input = new String(skus).trim();
+        for (String key : mapRegular.keySet()) {
+            input.replaceAll(key, "");
+        }
+        return input.length() != 0;
     }
 }
+
 
